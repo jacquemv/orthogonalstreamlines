f160ad9d6f67a35927bfa8b361586052afc40a2a
Vincent Jacquemet <vincent.jacquemet@umontreal.ca>
2023-11-30 11:20:05 -0500

    separate vertices structure from intersection

---
+++ orthogonalstreamlines/intersection/vertices.h
@@ -0,0 +1,27 @@
+#ifndef VERTICES_H_
+#define VERTICES_H_
+
+#define INVALID_INDEX 999999999
+
+class Vertices {
+public:
+    int size;
+    double* pos;
+    int* idtri;
+    char* sign;
+
+    Vertices();
+    ~Vertices();
+    void allocate(int capacity_);
+
+    void append(double* pos_, int idtri_, char sign_);
+    int remove_if_zero(int* flag); // flag must be of length at least 'size'
+    int renumber_indices(int n, int* indices); // indices has size n
+
+//private:
+    int capacity;
+    int* new_idx;
+    int size_new_idx;
+};
+
+#endif
